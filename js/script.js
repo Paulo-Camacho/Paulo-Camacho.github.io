81ee4447311f7d86e00670384d494bca6f9d4dcd
Paulo-Camacho <anthropologyparliament@gmail.com>
2025-09-19 13:02:10 -0700

    Working on the JavaScript code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Global variables
+let randomNumber;
+let attempts = 0;
+
+initializeGame();
+
+function initializeGame() {
+   randomNumber = Math.floor(Math.random() * 99) + 1;
+   console.log("randomNumber: " + randomNumber);
+
+   //hiding the Reset button
+   document.querySelector("#resetBtn").style.display = "none";
+   //adding focus to textbox
+   document.querySelector("#playerGuess").focus();
+}
